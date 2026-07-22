301a24d6acd7572fdcb47b106f9d699b18bf3701
RyanSuda-Code <sudaryan49@yahoo.com>
2026-07-22 22:09:50 +0900

    Update: added a function that takes the data from the TodoForm.

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -2,8 +2,15 @@ import "./MainSection.css";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 import TodoFilter from "./TodoFilter";
+import { useState } from "react";
 
-export default function MainSection() {
+export default function MainSection({ onSubmit }) {
+  const [todos, setTodos] = useState([]);
+
+  function addTodo(newTodo) {
+    setTodos((prevTodos) => [...prevTodos, newTodo]); //Take the current todo list, add this new todo at the end, and save the new list
+  }
+
   return (
     <div className="main-container">
       <TodoForm />
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,15 +1,24 @@
 import "./TodoForm.css";
+import { useState } from "react";
 
-export default function TodoForm() {
+export default function TodoForm({ onSubmit }) {
+  const [tasks, setTasks] = useState("");
+
   const handleSubmit = (e: React.SubmitEvent) => {
     //SubmitEvent - Used by TypeScript for autocomplete and type checking
     e.preventDefault();
+    onSubmit(tasks);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <span className="input-circle"></span>
-      <input type="text" placeholder="Create a new todo..." />
+      <input
+        type="text"
+        placeholder="Create a new todo..."
+        value={tasks}
+        onChange={(e) => setTasks(e.target.value)}
+      />
     </form>
   );
 }
