4c72e220637fef23ee7299e8ad310f2f0ddb6275
RyanSuda-Code <sudaryan49@yahoo.com>
2026-07-22 21:05:33 +0900

    Initial commit

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,8 +1,13 @@
 import "./TodoForm.css";
 
 export default function TodoForm() {
+  const handleSubmit = (e: React.SubmitEvent) => {
+    //SubmitEvent - Used by TypeScript for autocomplete and type checking
+    e.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <span className="input-circle"></span>
       <input type="text" placeholder="Create a new todo..." />
     </form>
