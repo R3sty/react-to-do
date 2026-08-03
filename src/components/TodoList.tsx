e561922d5c6a144df84ecb2dbed80ef059adad56
RyanSuda-Code <sudaryan49@yahoo.com>
2026-08-03 22:24:30 +0900

    Update: Design and layout of button and text now changes if task is completed or not.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import "./TodoList.css";
 import cross from "../assets/images/icon-cross.svg";
+import check from "../assets/images/icon-check.svg";
 
 export default function TodoList({ todoItems, onButtonClick }) {
   return (
@@ -10,8 +11,16 @@ export default function TodoList({ todoItems, onButtonClick }) {
             className="complete-btn"
             onClick={() => onButtonClick(task.id)}
           >
-            <span className="check-icon"></span>
-            <span>{task.text}</span>
+            {task.completed ? (
+              <span className="check-icon">
+                <img src={check} className="check" alt="check icon" />
+              </span>
+            ) : (
+              <span className="check-circle"></span>
+            )}
+            <span className={task.completed ? "completed" : ""}>
+              {task.text}
+            </span>
           </button>
           <button className="delete-btn">
             <img src={cross} />
