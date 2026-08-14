78774886ff3ff841ef72e05b77fc0b531a887e1c
RyanSuda-Code <sudaryan49@yahoo.com>
2026-08-15 00:12:08 +0900

    main element now changes to darker bg color whenever the theme button is clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
 import MainSection from "./components/MainSection";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
     <>
-      <Header />
-      <main>
+      <Header setDarkMode={setDarkMode} />
+      <main className={darkMode ? "dark" : "light"}>
         <MainSection />
       </main>
     </>
